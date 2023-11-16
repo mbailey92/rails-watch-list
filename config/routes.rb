902090d626e13b5fb86f3037d16b60d5029822a5
Michael Bailey <michaelbailey92@gmail.com>
2023-11-16 20:05:08 +0000

    movie list day 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "lists#index"
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies
 end
